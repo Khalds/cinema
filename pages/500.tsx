595bb951aa561d49721fafacbf5018a25da44402
khald <abv.khallid@mail.ru>
2023-05-15 20:05:37 +0300

    404 page and popular movies in sidebar

---
+++ pages/500.tsx
@@ -0,0 +1,12 @@
+import Heading from '@/components/ui/heading/Heading'
+
+import Meta from '@/utils/meta/Meta'
+
+const Error500 = () => {
+	return (
+		<Meta title="Page not found">
+			<Heading title="500 - Page Not Found"></Heading>
+		</Meta>
+	)
+}
+export default Error500
